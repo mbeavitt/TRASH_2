f57d0900f6ca7c2430e2c7671a0e5df2c6419f28
Michael Beavitt <mbeavitt@gmail.com>
2025-10-29 11:07:07 +0000

    getting rid of unnecessary sliding_window_distance variable

--- src/array/sequence_window_score.R
+++ src/array/sequence_window_score.R
@@ -6,13 +6,11 @@ sequence_window_score <- function(fasta_sequence, window_size, kmer = 10, output
 
   sequence_full_length <- length(fasta_sequence)
 
-  # window definition needs to be propagated into merge_windows() funtion
-  sliding_window_distance <- ceiling(window_size / 1) #if 1, it's off, use 4
-  if(sliding_window_distance >= sequence_full_length) {
+  if(window_size >= sequence_full_length) {
     starts <- 1
     ends <- sequence_full_length
   } else {
-    starts <- genomic_bins_starts(start = 1, end = sequence_full_length, bin_size = sliding_window_distance)
+    starts <- genomic_bins_starts(start = 1, end = sequence_full_length, bin_size = window_size)
     starts <- starts[starts < sequence_full_length]
     if (length(starts) == 1) {
       ends <- sequence_full_length
@@ -21,7 +19,7 @@ sequence_window_score <- function(fasta_sequence, window_size, kmer = 10, output
     }
     ends[ends > sequence_full_length] <- sequence_full_length
   }
-  # cat(seq_along(starts), "\n")
+
   # Divide into chunks
   scores <- NULL
   wins_per_chunk <- 100
